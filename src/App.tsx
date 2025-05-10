735bee71ff2dd0692e789ef236fc99dab678fffc
yujin5959 <jinjin17104@naver.com>
2025-05-10 18:15:31 +0900

    FEAT: 기본 레이아웃 설정 및 기본 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Map from './pages/Map';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <div>404 에러 발생 관리자에게 문의하세요!</div>,
+    children: [
+      {
+        path: '/map',
+        element: <Map />,
+      },
+      {
+        path: '/feed',
+        element: <div>피드입니다.</div>,
+      },
+      {
+        path: '/post',
+        element: <div>게시글 작성하기입니다.</div>,
+      },
+      {
+        path: '/chat',
+        element: <div>채팅입니다.</div>,
+      },
+      {
+        path: '/mypage',
+        element: <div>마이페이지입니다.</div>,
+      },
+    ],
+  },
+]);
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
